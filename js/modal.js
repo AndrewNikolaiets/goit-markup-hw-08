fb6c57ea56b4485cb4e8b795085c8e37d89a49bb
AndrewNikolaiets <nikolaiets.a.e@gmail.com>
2022-07-07 23:44:04 +0300

    DZ-8 Modalka 2

--- js/modal.js
+++ js/modal.js
@@ -14,3 +14,21 @@
       refs.body.classList.toggle("no-scroll");
   }
 })();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[mob-modal-open]"),
+    closeModalBtn: document.querySelector("[mob-modal-close]"),
+      modal: document.querySelector("[mob-modal]"),
+    body: document.querySelector("[body]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+      refs.body.classList.toggle("no-scroll");
+  }
+})();
+
